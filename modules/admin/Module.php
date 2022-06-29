e30561dbf9653bf3edfb799fedbb15481464bdd2
Pavel Sazhin <89504691297@mail.ru>
2022-06-29 16:48:05 +0500

    добавил модуль админки

---
+++ modules/admin/Module.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\modules\admin;
+
+use yii\filters\AccessControl;
+
+/**
+ * admin module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'app\modules\admin\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                // 'only' => ['login', 'logout', 'signup'],
+                'rules' => [
+                    // [
+                    //     'allow' => true,
+                    //     'actions' => ['login', 'signup'],
+                    //     'roles' => ['?'],
+                    // ],
+                    [
+                        'allow' => true,
+                        // 'actions' => ['logout'],
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
+    }
+}
