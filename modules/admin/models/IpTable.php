b71d7c9df699fae25033abd41c33dbc8d2ee8208
Pavel Sazhin <89504691297@mail.ru>
2022-06-29 16:45:36 +0500

    создал модели в админке

---
+++ modules/admin/models/IpTable.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace app\modules\admin\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "ip".
+ *
+ * @property int $id
+ * @property int $ip
+ * @property string $browser
+ * @property int $user_id
+ *
+ * @property UserName $user
+ * @property UserName[] $userNames
+ */
+class IpTable extends \yii\db\ActiveRecord
+{
+
+
+    /**
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'ip';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['ip', 'browser', 'user_id'], 'required'],
+            [['user_id'], 'integer'],
+            [['browser', 'ip'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'ip' => 'Ip',
+            'browser' => 'Browser',
+            'user_id' => 'User ID',
+        ];
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(UserName::className(), ['id' => 'user_id']);
+    }
+}
---
+++ modules/admin/models/UserName.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace app\modules\admin\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user_name".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $content
+ * @property string $created
+
+ */
+class UserName extends \yii\db\ActiveRecord
+{
+
+    public static function primaryKey()
+    {
+        return ["id"];
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user_name';
+    }
+
+    // public $ip;
+    // public $browser;
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'email', 'content'], 'required'],
+            [['email'], 'email'],
+            [['content'], 'string'],
+            [['created'], 'safe'],
+            [['name', 'email'], 'string', 'max' => 64],
+            ['name', 'match', 'pattern' => '/^[a-z0-9]+$/i', 'message' => 'Логин может содержать только латиницу и цифры!'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Логин',
+            'email' => 'Email',
+            'content' => 'Текст сообщения',
+            'created' => 'Дата создания отзыва',
+        ];
+    }
+
+    function afterSave($insert, $changedAttributes)
+    {
+        parent::afterSave($insert, $changedAttributes);
+
+        $modelIp = new IpTable();
+        $modelIp->ip = Yii::$app->request->userIP;
+        $data = explode(' ', Yii::$app->request->userAgent);
+        $modelIp->browser = $data[0] . " admin";
+        $modelIp->user_id = $this->id;
+        if ($modelIp->save()) {
+            Yii::$app->session->setFlash('success', 'Отзыв оставлен');
+        } else {
+            Yii::$app->setFlash('error', "Возникла ошибка при сохранении");
+        }
+    }
+
+
+    /**
+     * Gets query for [[Ip]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getIpTable()
+    {
+        return $this->hasOne(IpTable::className(), ['user_id' => 'id']);
+    }
+}
